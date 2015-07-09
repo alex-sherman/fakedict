f33ad4d7a18fa30075f75af7c7892e67be50911a
Alex Sherman <asherman1024@gmail.com>
2015-07-09 14:12:20 -0500

    Add a collection dict that will work on python lists

--- fakedict/__init__.py
+++ fakedict/__init__.py
@@ -1,3 +1,4 @@
 import fakedict
 FakeDict = fakedict.FakeDict
 JSONFile = fakedict.JSONFile
+CollectionDict = fakedict.CollectionDict
--- fakedict/fakedict.py
+++ fakedict/fakedict.py
@@ -62,3 +62,47 @@ class JSONFile(FakeDict):
         values = self.dict()
         del values[name]
         self._save_dict(values)
+
+class CollectionDict(FakeDict):
+    def __init__(self, collection,
+            col_key_getter = lambda item: item[0],
+            col_value_getter = lambda item: item[1],
+            col_value_setter = None,
+            col_initializer = lambda name, value: [name, value]):
+        self.col_key_getter = col_key_getter
+        self.col_value_getter = col_value_getter
+        self.col_value_setter = col_value_setter if col_value_setter != None else self._default_setter
+        self.col_initializer = col_initializer
+        self.collection = collection
+        FakeDict.__init__(self, self.getter, self.setter, self.key_getter, self.del_item)
+
+    def _find_index(self, name):
+        keys = self.key_getter()
+        if name not in keys:
+            raise KeyError(name)
+        return self.key_getter().index(name)
+
+    def _find_item(self, name):
+        return self.collection[self._find_index(name)]
+
+    def getter(self, name):
+        return self.col_value_getter(self._find_item(name))
+
+    def setter(self, name, value):
+        item = None
+        try:
+            item = self._find_item(name)
+            self.col_value_setter(item, value)
+        except KeyError:
+            item = self.col_initializer(name, value)
+            self.collection.append(item)
+
+    def _default_setter(self, item, value):
+        item[1] = value
+
+    def key_getter(self):
+        return [self.col_key_getter(item) for item in self.collection]
+
+    def del_item(self, name):
+        self.collection.pop(self._find_index(name))
+
